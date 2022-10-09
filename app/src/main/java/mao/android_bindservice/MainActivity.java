5ac3b1018f816c5ed1ce99c8eeb8649274b61ab8
mao <1296193245@qq.com>
2022-10-09 20:07:13 +0800

    android_BindService

--- app/src/main/java/mao/android_bindservice/MainActivity.java
+++ app/src/main/java/mao/android_bindservice/MainActivity.java
@@ -2,15 +2,91 @@ package mao.android_bindservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import mao.android_bindservice.service.MyService;
 
 public class MainActivity extends AppCompatActivity
 {
+
+
+    private MyService.MyBinder binder;
+
+    /**
+     * 标签
+     */
+    private static final String TAG = "MainActivity";
+
+    private final ServiceConnection conn = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            Log.d(TAG, "onServiceConnected: ");
+            binder = (MyService.MyBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            Log.d(TAG, "onServiceDisconnected: ");
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent();
+        intent.setAction("mao.android_bindservice.service.MyService");
+        intent.setPackage("mao.android_bindservice");
+
+        findViewById(R.id.Button1).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                bindService(intent, conn, Service.BIND_AUTO_CREATE);
+            }
+        });
+
+        findViewById(R.id.Button2).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                unbindService(conn);
+            }
+        });
+
+        findViewById(R.id.Button3).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                toastShow("Service的count的值为:" + binder.getCount());
+            }
+        });
+    }
+
+    /**
+     * 显示消息
+     *
+     * @param message 消息
+     */
+    private void toastShow(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
